921310a7956da52f7e071605c2d5c4ee3f2e1a54
Priyanshu sahu <priyanshusahupanagar@gmail.com>
2025-09-14 18:59:34 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,11 +39,11 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-13 left-0 w-full bg-white text-black shadow-md z-40 border-t border-gray-200">
-      <div className="max-w-[90%] w-full mx-auto flex justify-between items-center px-4 py-5 sm:px-6 lg:px-8">
+      <div className=" w-full mx-auto flex justify-between items-center px-4 py-5 sm:px-6 lg:px-8">
         {/* Left - Logo */}
         <div className="flex items-center space-x-2">
           <span className="text-black font-bold text-xl">CBM BlockExplorer</span>
-          <span className="text-gray-500 text-sm">A Scan Original</span>
+          {/* <span className="text-gray-500 text-sm">A Scan Original</span> */}
         </div>
 
         {/* Center - Menu Items */}
